edf775d70da35350a3d379c4004ed571a808e21d
plihal <plihal@cruxconsult.cz>
2022-08-03 15:38:24 +0200

    Deploy.sh

--- netlify-test-ForntEnd/deploy.sh
+++ netlify-test-ForntEnd/deploy.sh
@@ -1,11 +1,4 @@
-//pull the code from repo
-
 sudo git pull
 sudo npm i
 sudo rm -r dist /
 sudo ng build --prod
-sudo rm -r /var/www/<project Directory>
-sudo cp -r dist/ /var/www/<project Directory>
-sudo cp .htaccess /var/www/<project Directory> 
-
-//if you have .htaccess file this cmd will help
